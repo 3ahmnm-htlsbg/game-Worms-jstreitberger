ca32fe37356338bba24718c61c11640d3248a210
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2020-05-19 15:54:25 +0200

    create jumpKey & forwardKey

---
+++ Assets/MyGame/Script/WormController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+
+    public KeyCode jumpKey;
+
+    public KeyCode forwardKey;
+
+    public KeyCode backwardKey;
+
+    public Vector3 v;
+
+    public Vector3 y;
+
+    public Rigidbody z;
+
+    public Vector3 x;
+
+    public ForceMode f;
+
+
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            //dieser Code wird ausgeführt, sobald die in der Variable jumpkey definierte Taste gedrückt wird
+            Debug.Log("die JumpTaste wird gedrückt.");
+            z.AddForce(x, f);
+        }
+
+      
+
+        if (Input.GetKeyDown(forwardKey))
+        {
+            Debug.Log("die ForwardTaste wird gedrückt.");
+            z.AddForce(y, f);
+
+       
+        }
+
+        if (Input.GetKeyDown(backwardKey))
+        {
+            Debug.Log("die ForwardTaste wird gedrückt.");
+            z.AddForce(v, f);
+
+
+        }
+
+    }
+}
